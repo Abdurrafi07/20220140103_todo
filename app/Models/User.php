952cf0add3d646448c6f0bcb7359dd5f2a460836
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-03-17 13:47:56 +0700

    env, mvc, route, relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,8 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    
+    
     /**
      * Get the attributes that should be cast.
      *
@@ -45,4 +47,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function todos(): HasMany
+    {
+        return $this->hasMany(Todo::class);
+    }
+
 }
